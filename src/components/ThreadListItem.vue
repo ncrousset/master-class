cd2a368f51c39ca39787950f4009276643acbb32
Rudys NAtanael Acosta <natanael926@gmail.com>
2019-08-29 13:50:12 -0400

    Components, Props and Computed Properties, #1

---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="thread">
+    <div>
+      <p>
+        <a href="#">{{ thread.title }}</a>
+      </p>
+      <p class="text-faded text-xsmall">
+        By <a href="#">Joseph Kerr</a>, {{ thread.publishedAt }}
+      </p>
+    </div>
+
+    <div class="activity">
+      <p class="replies-count">
+        1 reply
+      </p>
+
+      <img class="avatar-medium" src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png" alt="">
+
+<!--      <div>-->
+<!--        <p class="text-xsmall">-->
+<!--          <a href="#">Bruce Wayne</a>-->
+<!--        </p>-->
+<!--        <p class="text-xsmall text-faded">2 hours ago</p>-->
+<!--      </div>-->
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'ThreadListItem',
+        props: {
+            thread: {
+                required: true,
+                type: Object
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
