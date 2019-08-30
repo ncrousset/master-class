ff064d3066687e4e1d7ad74e02614a2e28abc24d
Rudys NAtanael Acosta <natanael926@gmail.com>
2019-08-29 23:26:57 -0400

    Handling not found pages

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col-full">
+    <h1>Not Found</h1>
+    <p>Oooops, we couldn't find what you are looking for. Why don't you </p>
+    <router-link :to="{name:'Home'}">
+      go home instead?
+    </router-link>
+  </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+  h1{
+    font-size: 100px;
+  }
+  p{
+    font-size: 50px;
+    font-weight: 100;
+  }
+  div{
+    text-align: center;
+  }
+</style>
